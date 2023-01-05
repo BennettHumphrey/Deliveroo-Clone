0fa5c618209c939b312616717188397e08ac9c92
Bennett Humphrey <bennetthumphrey98@gmail.com>
2023-01-05 08:26:12 -0500

    finish main features

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,28 +1,47 @@
 import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { ArrowLeftIcon, ChevronRightIcon, QuestionMarkCircleIcon, StarIcon } from 'react-native-heroicons/solid'
 import { urlFor } from '../sanity'
 import { MapPinIcon } from 'react-native-heroicons/outline'
 import DishRow from '../components/DishRow'
 import BasketIcon from '../components/BasketIcon'
+import { useDispatch } from 'react-redux'
+import { setRestaurant } from '../features/restaurantSlice'
 
 const RestaurantScreen = () => {
 
     const navigation = useNavigation()
+    const dispatch = useDispatch()
+
     const {
-        params: {
-        id,
-        imgUrl,
-        title,
-        rating,
-        genre,
-        address,
-        short_description,
-        dishes,
-        long,
-        lat,
+            params: {
+            id,
+            imgUrl,
+            title,
+            rating,
+            genre,
+            address,
+            short_description,
+            dishes,
+            long,
+            lat,
         }} = useRoute();
+
+        useEffect(() => {
+            dispatch(setRestaurant({
+                id,
+                imgUrl,
+                title,
+                rating,
+                genre,
+                address,
+                short_description,
+                dishes,
+                long,
+                lat,
+            }));
+        }, [dispatch])
 
         useLayoutEffect(() => {
             navigation.setOptions({
@@ -73,21 +92,21 @@ const RestaurantScreen = () => {
             </TouchableOpacity>
         </View>
 
-        <View  >
+        <View className="pb-36" >
             <Text className="px-4 pt-6 mb-3 font-bold text-xl" >Menu</Text>
-        </View>
 
-        {/* Dish rows */}
-        {dishes.map((dish) => (
-            <DishRow 
-                key={dish._id}
-                id={dish._id}
-                name={dish.name}
-                description={dish.short_description}
-                price={dish.price}
-                image={dish.image}
-            />
-        ))}
+            {/* Dish rows */}
+            {dishes.map((dish) => (
+                <DishRow 
+                    key={dish._id}
+                    id={dish._id}
+                    name={dish.name}
+                    description={dish.short_description}
+                    price={dish.price}
+                    image={dish.image}
+                />
+            ))}
+        </View>
 
     </ScrollView>
     </>
